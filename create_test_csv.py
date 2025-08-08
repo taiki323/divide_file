74bee7c5d74c5bdbb03c24016b3c4f4085a47a3e
yunreah <unitia0323@gmail.com>
2025-08-08 22:15:38 +0900

    変数で設定

--- create_test_csv.py
+++ create_test_csv.py
@@ -1,10 +1,8 @@
 import csv
 
 def create_test_csv():
-    """
-    origin.csvファイルに商品コード（A1からA10000）と入札価格（25）のデータを10000行作成する
-    """
     filename = 'origin.csv'
+    num_records = 600000  # データ行数を変数として定義
     
     # CSVファイルを書き込みモードで開く
     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
@@ -13,14 +11,14 @@ def create_test_csv():
         # ヘッダーを書き込み
         writer.writerow(['商品コード', '入札価格'])
         
-        # 10000行のデータを作成
-        for i in range(1, 10001):
+        # データを作成
+        for i in range(1, num_records + 1):
             product_code = f'A{i}'
             bid_price = 25
             writer.writerow([product_code, bid_price])
     
-    print(f'{filename}に10000行のデータを作成しました。')
-    print('商品コード: A1 から A10000')
+    print(f'{filename}に{num_records}行のデータを作成しました。')
+    print(f'商品コード: A1 から A{num_records}')
     print('入札価格: 25（固定）')
 
 if __name__ == '__main__':
